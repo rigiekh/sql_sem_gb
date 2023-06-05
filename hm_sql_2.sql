585df54153e54087689c6a516a153496bfdb9c6f
com_rhh <94183067+rigiekh@users.noreply.github.com>
2023-06-05 13:24:03 +0200

    Update hm_sql_2.sql

--- hm_sql_2.sql
+++ hm_sql_2.sql
@@ -1,26 +1,25 @@
-DROP DATABASE IF EXISTS hw_sql_2;
-
-CREATE DATABASE IF NOT EXISTS hw_sql_2;
+CREATE DATABASE hw_sql_2;
 
 USE hw_sql_2;
 
 -- 1. Используя операторы языка SQL, создайте таблицу “sales”. Заполните ее данными.
 
-CREATE TABLE IF NOT EXISTS sales
+-- Создание таблицы "sales"
+
+CREATE TABLE sales
 (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     order_date DATE NOT NULL,
     count_product INT NOT NULL
 );
 
-INSERT INTO sales(order_date, count_product)
-VALUES
-	(DATE '2022-01-01', 156),
-	(DATE '2022-01-02', 180),
-	(DATE '2022-01-03', 21),
-	(DATE '2022-01-04', 124),
-	(DATE '2022-01-05', 341);
-
+-- Заполнение таблицы данными
+INSERT INTO sales (id, order_date, count_product)
+VALUES (1, '2022-01-01', 156),
+       (2, '2022-01-02', 180),
+       (3, '2022-01-03', 21),
+       (4, '2022-01-04', 124),
+       (5, '2022-01-05', 341);
 
 /*
 2.  Для данных таблицы “sales” укажите тип заказа в зависимости от кол-ва : 
@@ -29,16 +28,20 @@ VALUES
 больше 300 - Большой заказ
 */
 
+-- Указываем тип заказа для данных из таблицы sales
+
 SELECT 
 	id,
     CASE
 	WHEN count_product < 100 THEN "Маленький заказ"
-        WHEN count_product BETWEEN 100 AND 300 THEN "Средний заказ"
-        ELSE "Большой заказ"
+    WHEN count_product BETWEEN 100 AND 300 THEN "Средний заказ"
+    ELSE "Большой заказ"
     END AS "Тип заказа"
 FROM sales;
 
 -- 3. Создайте таблицу “orders”, заполните ее значениями
+
+-- Создание таблицы orders
 
 CREATE TABLE IF NOT EXISTS orders
 (
@@ -48,6 +51,8 @@ CREATE TABLE IF NOT EXISTS orders
     order_status VARCHAR(20)
 );
 
+-- Заполнение таблицы oders данными
+
 INSERT INTO orders (employee_id, amount, order_status)
 VALUES
 ('s03', 15.00, "OPEN"),
@@ -56,11 +61,13 @@ VALUES
 ('e02', 22.18, "OPEN"),
 ('e04', 9.50, "CANCELLED");
 
+-- Вывод данных из таблицы orders с добавлением полного статуса заказа
+
 SELECT
-	id,
-        employee_id,
-        amount,
-        order_status,
+    id,
+    employee_id,
+    amount,
+    order_status,
 	CASE
 	WHEN order_status = "OPEN" THEN "Order is in open state"
         WHEN order_status = "CLOSED" THEN "Order is closed"
@@ -68,10 +75,3 @@ SELECT
         ELSE ""
     END AS full_order_status
 FROM orders;
-
-
-
-
-
-
- 
